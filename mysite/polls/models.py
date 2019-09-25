2726d8664843247ec6a3c7d9c35060d51f8ca198
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-09-25 18:46:52 -0500

    added a function to question that tells you in the question was posted within the last day

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
@@ -12,3 +15,5 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 	def __str__(self):
 		return self.choice_text
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
