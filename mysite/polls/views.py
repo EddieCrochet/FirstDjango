4b106c7868e2ba55735ec0e521efab53e8fee910
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-10-26 14:24:30 -0500

    Changed the detail view to raise an http404 exception if the requested question ID doesn't exist

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-from django.template import loader
-
+#from django.template import loader
+#only needed this to import the template
+#no need after the render() method
+from django.http import Http404
 from .models import Question
 
 #index method lists latest 5 questions by default now
@@ -18,7 +19,13 @@ def index(request):
 	#return HttpResponse(output)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	#just a simple one line return below
+	#return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Qustion.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist!")
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
